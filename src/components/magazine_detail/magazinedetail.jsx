f6c9c778a133f753e0d1229ab27bf000b1138ab7
Anoulak <tomanoulak@gmail.com>
2024-06-03 14:38:57 +0700

    update 3/6/2024

--- src/components/magazine_detail/magazinedetail.jsx
+++ src/components/magazine_detail/magazinedetail.jsx
@@ -6,8 +6,8 @@ export default function magazinedetail() {
       <div className="container pt-100 pb-30">
         <iframe
           allowfullscreen="true"
-          // src="https://online.fliphtml5.com/uamqb/daqs/"
-          src="https://designrr.page/?id=359664&token=748970862&type=FP&h=5092"
+          src="https://heyzine.com/flip-book/724fae8541.html"
+          // src="https://designrr.page/?id=359664&token=748970862&type=FP&h=5092"
           height="700"
           width="100%"
           frameborder="0"
