038b31f69b6157f41fc44f7ed2f491e3be3edaf6
EDL07 <edl07@EDL07s-MacBook-Pro.local>
2026-02-25 14:16:41 +0700

    25022026

--- src/components/iso/iso.jsx
+++ src/components/iso/iso.jsx
@@ -2,21 +2,20 @@
 import React from "react";
 import Image from "next/image";
 
-import img1 from "@/public/images/iso/EDL_Quality_Policy1.jpg";
-import img2 from "@/public/images/iso/logoiso.jpeg";
+import img1 from "@/public/images/iso/60x40-2026.png";
+import img2 from "@/public/images/iso/GCC-9001.png";
 
 const Organize = () => {
   return (
     <>
       <div className="container mt-60 mb-60 text-center wow tpfadeUp">
         <div className="row">
-        <div className="col-lg-6 col-md-6 col-6">
-          <Image src={img1} className="img-fluid" alt="theme-pure" />
-        </div>
-        <div className="col-lg-6 col-md-6 col-6">
-          <Image src={img2} className="img-fluid" alt="theme-pure" style={{ width: '470px' }}/>
-        </div>
-
+          <div className="col-lg-6 col-md-6 col-6">
+            <Image src={img1} className="img-fluid" alt="theme-pure" />
+          </div>
+          <div className="col-lg-6 col-md-6 col-6">
+            <Image src={img2} className="img-fluid" alt="theme-pure"/>
+          </div>
         </div>
       </div>
     </>
